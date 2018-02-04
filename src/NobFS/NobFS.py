1fb4b7b214b1843ee89abcbb57c0f07946b943fb
ATM@006 <18829897162@163.com>
2018-02-04 16:42:14 +0800

    change the directory structure

---
+++ src/NobFS/NobFS.py
@@ -0,0 +1,9 @@
+#print 'NobFS ...'
+def func_test():
+	print 'NobFS ...'
+
+def NobFS():
+	func_test()
+
+if __name__=="__main__":
+	func_test()
---
+++ src/iface/iface.py
@@ -0,0 +1,12 @@
+#!/usr/lib/python2.7
+
+
+def func_test():
+	print 'iface ...'
+	print 'XXX'
+
+def iface():
+	func_test()
+
+if __name__=="__main__":
+	func_test()
